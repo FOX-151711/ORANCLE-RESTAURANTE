1b19cae5a988491be792c07ac74697849a590300
varo01 <87771046+varo01@users.noreply.github.com>
2024-07-23 11:10:15 -0600

    Update --Funciones.sql

--- --Funciones.sql
+++ --Funciones.sql
@@ -15,3 +15,22 @@ BEGIN
     RETURN v_Total;
 END;
 /
+--Funcion para obtener informacion de una reservacion
+CREATE OR REPLACE FUNCTION ObtenerInfoReservacion(
+    p_TN_NumReservacion INT
+) RETURN SYS_REFCURSOR IS
+    v_Cursor SYS_REFCURSOR;
+BEGIN
+    OPEN v_Cursor FOR
+        SELECT r.TN_NumReservacion, r.TN_IdCliente, r.TN_IdMesa, r.TN_IdMenu, r.TN_Cantidad, r.TF_FecReserva,
+               c.TC_Nombre, c.TC_Ap1, c.TC_Ap2, c.TC_NumTelefono, c.TC_CorreoElectronico,
+               m.TC_DscMesa,
+               menu.TC_DscMenu, menu.TD_Precio
+        FROM TREST_RESERVACION r
+        JOIN TREST_CLIENTES c ON r.TN_IdCliente = c.TN_IdCliente
+        JOIN TREST_MESAS m ON r.TN_IdMesa = m.TN_IdMesa
+        JOIN TREST_MENU menu ON r.TN_IdMenu = menu.TN_IdMenu
+        WHERE r.TN_NumReservacion = p_TN_NumReservacion;
+
+    RETURN v_Cursor;
+END;
